68ddd439145da99c688f578bf7677bfdcc17fa56
KyungWon_J <103038606+kw99j10@users.noreply.github.com>
2023-05-27 16:20:43 +0900

    practice -level2

---
+++ level2/practice/phoneNumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        var h=new HashSet<String>();
+        for(String s:phone_book){
+            h.add(s);
+        }
+        for(String s:phone_book){
+    
+          //부분문자열을 이용
+            for(int i=0;i<s.length();i++){
+                if(h.contains(s.substring(0,i))){
+                    answer=false;
+                }
+            }
+        }
+        return answer;
+    }
+}
