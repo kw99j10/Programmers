f7169fbd232171b992a5b49924a0e42d6a18135d
KyungWon_J <103038606+kw99j10@users.noreply.github.com>
2023-06-10 21:15:27 +0900

    level2-bruteforce

--- level2/practice/카펫.java
+++ level2/practice/카펫.java
@@ -15,7 +15,8 @@ class Solution {
             //가로의 길이 역시 3보다 크거나 같으므로
             if(k%i==0 && j>=3){
                 
-                //(구한 가로-2)x(구한 세로-2)의 크기가 yellow의 개수여야 함
+                //구한 가로와 세로는 전체 타일의 가로와 세로이고 이는 yellow 타일보다 +2이므로, 
+                //(구한 가로-2)x(구한 세로-2)의 크기가 yellow의 가로,세로 길이여야함
                 if((i-2)*(j-2)==yellow){
                     answer[0]=Math.max(i,j);
                     answer[1]=Math.min(i,j);
