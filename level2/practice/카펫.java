8654c9deec99d66d7bfbbf3cc7e708f257e0bb96
KyungWon_J <103038606+kw99j10@users.noreply.github.com>
2023-06-10 20:28:11 +0900

    level2-bruteforce

---
+++ level2/practice/카펫.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+      
+        //가로와 세로를 리턴하는 배열 (가로>=세로)
+        int[] answer = new int[2];
+        
+        //brown과 yellow의 최소로 하는 약수 쌍을 구함
+        int k=brown+yellow;
+        
+      
+        //세로의 최소길이는 3이므로 3부터 시작
+        for(int i=3;i<k;i++){
+            int j=k/i;
+            
+            //가로의 길이 역시 3보다 크거나 같으므로
+            if(k%i==0 && j>=3){
+                
+                //가로x세로의 크기가 yellow의 개수여야 함
+                if((i-2)*(j-2)==yellow){
+                    answer[0]=Math.max(i,j);
+                    answer[1]=Math.min(i,j);
+                }
+            }
+        }
+        return answer;
+    }
+}
