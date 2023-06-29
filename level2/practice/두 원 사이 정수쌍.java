359adb2d16ad8f39f1c2e253a43ed9f38c0eae99
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-06-29 23:50:23 +0900

    level2-practice

--- level2/practice/두 원 사이 정수쌍.java
+++ level2/practice/두 원 사이 정수쌍.java
@@ -7,8 +7,8 @@ class Solution {
 
         for (int i = 1; i <= r2; i++) {
 
-            int distance1=(int)Math.floor(Math.sqrt((result2-Math.pow(i,2)))); //두 점 사이의 거리가 result2보다 작거나 같은 정수여야 함
-            int distance2=(int)Math.ceil(Math.sqrt((result1-Math.pow(i,2))));  //두 점 사이의 거리가 result1보다 크거나 같은 정수여야 함
+            double distance1=(long)Math.floor(Math.sqrt((result2-Math.pow(i,2)))); //두 점 사이의 거리가 result2보다 작거나 같은 정수여야 함 (answer의 타입이 long이므로 미리 형변환 필요)
+            double distance2=(long)Math.ceil(Math.sqrt((result1-Math.pow(i,2))));  //두 점 사이의 거리가 result1보다 크거나 같은 정수여야 함
             answer += distance1 - distance2 + 1;
         }
         answer*=4; //4사분면이므로 구한 answer에 x 4
