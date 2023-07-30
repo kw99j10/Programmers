b6024e5961c772c04d91b68ffb6358aedc7ac431
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-07-30 13:32:38 +0900

    level2-practice

--- level2/practice/점 찍기.java
+++ level2/practice/점 찍기.java
@@ -4,7 +4,7 @@ class Solution {
     
         for (int i = 0; i <= d; i += k) {
             long distance_xy=(long)(Math.sqrt((long)d*d-(long)i*i))/k+1; 
-            //기준좌표인 d에서 k만큼의 좌표까지의 거리를 구하는 것과 같다. 이 때 거리가 같은 경우도 세야하므로 +1을 해준다.
+            //기준 좌표인 d에서 k 만큼의 좌표까지의 거리를 구하는 것과 같다. 이때 거리가 같은 경우도 세야 하므로 +1을 함
             answer+=distance_xy;
         }
         return answer;
