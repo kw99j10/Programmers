50808f336229a460699e36c672d050f5163e1a27
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-07-31 14:33:41 +0900

    level2-practice

---
+++ level2/practice/마법의 엘리베이터.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while (storey != 0) {
+            int n = storey % 10; //절댓값이 10c (c ≥ 0 인 정수) 형태인 정수들이 적힌 버튼들 이므로 현재 층수를 10으로 나눈 값을 확인
+
+            
+            if (n >= 6 || (n == 5 && storey / 10 % 10 >= 5)) {
+                storey += 10 - n; //나머지가 6 이상 몫이 5인 수의 나머지가 5 이상인 경우 현재 층 수를 증가 시킴
+                answer += 10 - n;
+            }
+            else {
+                answer += n; //나머지가 5이하, 몫이 5인 수의 나머지가 4이하인 경우는 현재 층 수를 그대로 answer에 더
+            }
+            storey /= 10; 
+        }
+        
+        return answer;
+    }
+}
