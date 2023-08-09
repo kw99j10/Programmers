346b48d0abb230c4309162911c7dcb9c5911b59b
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-08-09 13:38:43 +0900

    level2-pratice
    update info

--- level2/practice/JadenCaseString.java
+++ level2/practice/JadenCaseString.java
@@ -4,11 +4,11 @@ class Solution {
         String []st=s.split(" ");
         for (int i=0;i<st.length;i++){
 
-            if (st[i].length()==0)
-                sb.append(" ");
+            if (st[i].length()==0) == (st[i].equals("")
+                sb.append(" "); => 공백으로 나누었을 때 그 길이가 0: 공백이 연속해서 나온 것으로 그 길이가 0이므로 " "(공백)을 stringBuilder에 더함
 
             else
-                sb.append(st[i].substring(0, 1).toUpperCase()).append(st[i].substring(1).toLowerCase()).append(" ");
+                sb.append(st[i].substring(0, 1).toUpperCase()).append(st[i].substring(1).toLowerCase()).append(" "); //일반적인 경우 첫 글자를 대문자로, 아닐 경우 소문자로 변환
         }
          
         //마지막 부분 문자열이 공백으로 올 경우의 수를 생각해야 함
