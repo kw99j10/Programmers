aa634d18c36589d3a588572c56e513e0db2ad1ae
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-09-05 19:23:22 +0900

    level2-greedy
    ★★★해설 추가

--- level2/practice/조이스틱.java
+++ level2/practice/조이스틱.java
@@ -13,21 +13,26 @@ class Solution {
         }
 
         int next; //현재 값 기준 다음 값
-        int move = name.length() - 1; //좌, 우 움직임 변수
+        int move = name.length() - 1; //좌, 우 움직임 변수(기본 값은 순서대로 처리)
 
         //좌,우 값 비교하여 좌,우 조이스틱 조작 경우의 수
         for (int i = 0; i < name.length(); i++) {
 
             next = i + 1;
 
-            //연속되는 A의 개수 확인(좌,우 이동방향 결정)
+            //연속되는 A의 개수 확인
+            //(좌,우 이동방향 결정: 연속된 A의 개수 기준 정방향, 역방향 실행)
             while (next < name.length() && name.charAt(next) == 'A') {
                 next += 1;
             }
 
-            move = Math.min(move, i * 2 + name.length() - next); //정주행, 역주행 판단
-            move = Math.min(move, (name.length() - next) * 2 + i);
-            //ex) BBBBAAB와 같이 연속된 A개수 기준 앞 부분이 뒷 부분보다 길다면 뒷 부분부터 계산하는 것이 더 효율적
+            //i*2하는 이유: A가 나오기 전까지의 앞 부분(왕복)
+            //name.length()-next: A가 나오기전 까지의 뒷 부분, 기존 move 변수는 순서대로 세는 경우의 수
+            //계산 순서: i*2 부분 -> name.length()-next
+            move = Math.min(move, i * 2 + (name.length() - next));
+            
+            // "BBBBAAAAAAAB" 처럼 뒤로 세는 경우의 수만 존재할 때를 고려
+            move = Math.min(move, i + 2 * (name.length() - next));
         }
         answer += move;      
         return answer;
