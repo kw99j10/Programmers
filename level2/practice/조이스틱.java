217dcce2006ee6c917cb4c201ac57f09a903275c
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-09-05 14:40:46 +0900

    level2-greedy

--- level2/practice/조이스틱.java
+++ level2/practice/조이스틱.java
@@ -27,7 +27,7 @@ class Solution {
 
             move = Math.min(move, i * 2 + name.length() - next); //정주행, 역주행 판단
             move = Math.min(move, (name.length() - next) * 2 + i); //처음부터 역주행할 경우를 판단
-            //ex) BBBBAAB와 같이 연속된 A개수 기준 앞 부분이 뒷 부분보다 길다면 앞 부분부터 계산
+            //ex) BBBBAAB와 같이 연속된 A개수 기준 앞 부분이 뒷 부분보다 길다면 뒷 부분부터 계산
         }
         answer += move;      
         return answer;
