2b27d0e3f309a3438f7fe4c79ac1fd4d098fec7e
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-09-22 00:01:46 +0900

    level4-dp

--- level4/practice/올바른 괄호 갯수.java
+++ level4/practice/올바른 괄호 갯수.java
@@ -4,15 +4,17 @@ class Solution {
         
         int []dp=new int[15]; //주어진 괄호 쌍의 개수 범위
 
+        //카탈란 수: 1,1,2,5,14, ....
         dp[0]=1;
         dp[1]=1;
 
-        //카탈란 수: 1,1,2,5,14, ....
+        //ex) 만약 n이 3이라면 dp[2]*dp[0]+dp[1]*dp[1]+dp[0]*dp[2] 형태로써 이전 항들을 이용하여 2중 for 문으로 dp를 수행할 수 있음
         for (int i = 2; i < 15; i++) {
            for(int j = 0; j < i; j++) {
-                dp[i] += dp[i - j - 1] * dp[j];
+                dp[i] += dp[i - j - 1] * dp[j]; 
             }
         }
+        
         /*for (int i = 0; i < dp.length; i++) {
             System.out.print(dp[i] + " ");
         }*/
