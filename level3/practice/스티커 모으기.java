c14a80a68a708f65ff1499211b93ee42c4239fde
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-09-22 17:25:02 +0900

    level3-dp
    2 solve

--- level3/practice/스티커 모으기.java
+++ level3/practice/스티커 모으기.java
@@ -36,3 +36,40 @@ class Solution {
         return answer;
     }
 }
+
+
+//2번째 풀이
+class Solution {
+    public int solution(int sticker[]) {
+        int answer = 0;
+
+        //길이가 1 or 2일 경우 각각 예외 처리
+        if(sticker.length==1){
+            return sticker[0];
+        }
+        if(sticker.length==2){
+            return Math.max(sticker[0],sticker[1]);
+        }
+        
+        int[] st1 = new int[sticker.length]; //첫 번째 스티커부터 제거
+        int[] st2 = new int[sticker.length]; //두 번째 스티커부터 제거하는 경우
+
+        st1[0] = sticker[0];
+        st1[1] = Math.max(st1[0], sticker[1]);
+        for (int i = 2; i < sticker.length-1; i++) {
+            st1[i] = Math.max(st1[i - 1], st1[i - 2] + sticker[i]); //2개 간격으로 최대 경우의 수 비교
+        }
+        st1[st1.length-1]=st1[st1.length-2]; //첫 번째 스티커 제거 시 마지막 스티커 제거하지 못하므로 이전 항과 같은 값을 가짐
+
+        st2[0]=sticker[1];
+        st2[1]=Math.max(st2[0],sticker[2]);
+
+        for(int i=2;i<sticker.length-1;i++){
+            st2[i]=Math.max(st2[i-1],st2[i-2]+sticker[i+1]); //첫 번째 스티커를 제거하지 않았으므로 한 칸씩 증가하여 값을 비교
+        }
+        st2[st2.length-1]=st2[st2.length-2]; //마지막 dp(값)에 이전 항과 같은 값 대입
+        
+        answer=Math.max(st1[st1.length-1],st2[st2.length-1]);
+        return answer;
+    }
+}
