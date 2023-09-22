00d8666cf71b1753759e1148290a46db57ee1ab6
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-09-22 17:50:14 +0900

    level4-dp

--- level4/practice/올바른 괄호 갯수.java
+++ level4/practice/올바른 괄호 갯수.java
@@ -8,7 +8,7 @@ class Solution {
         dp[0]=1;
         dp[1]=1;
 
-        //ex) 만약 n이 3이라면 dp[2]*dp[0]+dp[1]*dp[1]+dp[0]*dp[2] 형태로써 이전 항들을 이용하여 2중 for 문으로 dp를 수행할 수 있음
+        //ex) 만약 n이 3이라면 dp[3]=dp[2]*dp[0]+dp[1]*dp[1]+dp[0]*dp[2] 형태로써 이전 항들을 이용하여 2중 for 문으로 dp를 수행할 수 있음
         for (int i = 2; i < 15; i++) {
            for(int j = 0; j < i; j++) {
                 dp[i] += dp[i - j - 1] * dp[j]; 
