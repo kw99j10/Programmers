7bdceae36a402bd5ba931ce5444d7c73ebc2b3a8
JungKyungWon <103038606+kw99j10@users.noreply.github.com>
2023-10-14 00:05:29 +0900

    update comment
    해설 추가, 원의 방정식, math

--- level2/practice/두 원 사이 정수쌍.java
+++ level2/practice/두 원 사이 정수쌍.java
@@ -5,11 +5,11 @@ class Solution {
         double result1=Math.pow(r1,2); //반지름이 r1인 원의 원점과의 거리
         double result2=Math.pow(r2,2); //반지름이 r2인 원의 원점과의 거리
 
+        //반복문이 1부터 시작하는 이유: 0부터 시작하면 x축, y축 2번씩 포함되어 중복 계산이 발생함
         for (int i = 1; i <= r2; i++) {
-
-            double distance1=(long)Math.floor(Math.sqrt((result2-Math.pow(i,2)))); //두 점 사이의 거리가 result2보다 작거나 같은 정수여야 함 (answer의 타입이 long이므로 미리 형변환 필요)
-            double distance2=(long)Math.ceil(Math.sqrt((result1-Math.pow(i,2))));  //두 점 사이의 거리가 result1보다 크거나 같은 정수여야 함
-            answer += distance1 - distance2 + 1;
+            double y1=(long)Math.floor(Math.sqrt((result2-Math.pow(i,2)))); //큰 원의 y값이 (r*2-i*2)보다 작거나 같은 정수여야 함
+            double y2=(long)Math.ceil(Math.sqrt((result1-Math.pow(i,2))));  //작은 원의 y값이 (r*2-i*2)보다 크거나 같은 정수여야 함
+            answer += y1 - y2 + 1; //큰 원에서 작은 원을 빼야 두 원 사이의 정수 쌍이 나온다. 이 후 작은 원과 큰 원이 겹치는 부분을 +1 해줘야 함
         }
         answer*=4; //4사분면이므로 구한 answer에 x 4
       
